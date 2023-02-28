45c836d849f24bf11024a54352d663365ed9033b
RonelShorPacker <ronel.packer1@gmail.com>
2023-02-28 12:11:04 +0200

    Almost finished

--- Encode.py
+++ Encode.py
@@ -5,11 +5,11 @@ import face_recognition as fr
 
 
 def getEncodings(img_dict):
-    encodings = []
-    for img in img_dict.values():
+    encodings = {}
+    for name, img in zip(img_dict.keys(), img_dict.values()):
         cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = fr.face_encodings(img)[0]
-        encodings.append(encode)
+        encodings[name] = encode
 
     return encodings
 
@@ -20,6 +20,7 @@ def readDatabase():
     for name in images_names:
         img_dict[name] = cv2.imread(path+name)
     return img_dict
+
 
 if __name__=="__main__":
     img_dict = readDatabase()
--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+import numpy as np
 import cv2
-
+import pickle
+import face_recognition as fr
+import cvzone
+import os
 
 def main():
-    cap = cv2.VideoCapture(0)
+    with open('encodings.pickle', 'rb') as handle:
+        img_dict_encode = pickle.load(handle)
+    # cap = cv2.VideoCapture(0)
     while True:
-        suc, image = cap.read()
+        # suc, image = cap.read()
+        image = cv2.imread("img.png")
+        img_small = cv2.resize(image, dsize=(0, 0), fx=0.25, fy=0.25)
+        img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+
+        faces_locs = fr.face_locations(img_small)
+        encoded_faces = fr.face_encodings(img_small, faces_locs)
+
+        for encoded_face, face_loc in zip(encoded_faces, faces_locs):
+            match = fr.compare_faces(list(img_dict_encode.values()), encoded_face)
+            dists = fr.face_distance(list(img_dict_encode.values()), encoded_face)
+            index = np.argmin(dists)
+            if match[index]:
+                y1, x2, y2, x1 = face_loc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                bbox = x1, y1, x2-x1, y2 - y1
+                cvzone.cornerRect(image, bbox, rt=0)
+                cv2.putText(image, f'{os.path.splitext(list(img_dict_encode.keys())[index])[0]}', (x1, y1 - 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 0, 0))
         cv2.imshow("Face", image)
         cv2.waitKey(1)
 
